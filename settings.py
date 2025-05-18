5e77c60d5dbda924ca01ce50c771d320f5ade221
Ajit Rawat <ajit74043@gmail.com>
2025-05-18 07:12:26 +0530

    Use .env for secrets

--- settings.py
+++ settings.py
@@ -100,8 +100,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ajit74043@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Ideally load from .env file
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') 
 import os
 
 from dotenv import load_dotenv
@@ -111,14 +111,6 @@ load_dotenv()  # take environment variables from .env.
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-
-
-
-
-
-
-
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
